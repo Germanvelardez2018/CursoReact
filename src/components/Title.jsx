0cc286d8bf7f8b2649f3a2f365a161bcfde6e945
German Velardez <germanvelardez16@gmail.com>
2022-12-11 16:58:33 -0300

    update

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,11 +1,17 @@
 import { Fragment } from "react"
 
+const title = "El titulo principal"
 
-export const Title = () => {
+
+
+export const Title = (props) => {
+  
+  console.log(props.title)
+  
   return (
     <Fragment>
-       <header><h1>Titulo</h1></header>
-        <h3> Subtitulo</h3>
+       <header><h1>{title} </h1></header>
+        <h3> Subtitulo:{props.title} </h3>
     </Fragment>
    
   )
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import {App} from './components/App.jsx';
 import {Title} from './components/Title'
 
 
+import './css/index.css'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Title title = 'titulo'/>
+        <Title/>
         <App/>
     </React.StrictMode>
 );
