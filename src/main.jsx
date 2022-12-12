d0419049bb1133a4a58f9a33a34d8bf617731c5b
German Velardez <germanvelardez16@gmail.com>
2022-12-12 18:11:26 -0300

    create CounterApp component

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import  React from "react";
 import  ReactDOM  from "react-dom/client";
 import {App} from './components/App.jsx';
+import { CounterApp } from "./components/CounterApp.jsx";
 
-import {Title} from './components/Title'
 
 
 import './css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Title title="title of my aplication"/>
+        <CounterApp/>
         <App/>
     </React.StrictMode>
 );
